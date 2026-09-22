3032b5fc9ec7204098946b079913c9b4971d974c
komal vaishnav <komalvaishnav2812@gmail.com>
2026-09-22 07:31:03 +0000

    Add article creation form and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
     def show
         # if i use @article = Article.find_by(id: params[:id]) it will return nil
         # if i use @article = Article.find(params[:id]) it will return the respective id (if exists) or an error
@@ -7,8 +6,27 @@ class ArticlesController < ApplicationController
         # binding.break # Stops the execution and lets you debug
         @article = Article.find(params[:id]) 
     end
-
     def index 
         @articles = Article.all
+    end
+    def new 
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+        # render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article was successfully created."
+            redirect_to @article  # or redirect_to article_path(@article)
+        # render plain: params[:article]
+        else 
+            render 'new'
+        end
+
+    end
+    private
+    def article_params
+        # params.require(:article).permit(:title, :description)
+        params.expect(article:[:title,:description])
     end
 end
